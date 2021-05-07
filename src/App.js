58901bc81ef71b1c7162e76277e78183986e10bb
prpwien <mail@prp.wien>
2021-05-07 14:05:21 +0200

    :sparkles: add App State

--- src/App.js
+++ src/App.js
@@ -7,18 +7,15 @@ import * as BooksAPI from "./BooksAPI";
 //Components
 import { BookShelfComponent } from "./components/BookShelfComponent";
 
-console.log(BooksAPI.getAll([1]));
+console.log(BooksAPI.getAll());
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false,
     books: BooksAPI.getAll(),
+    currentlyReading: null,
+    wantedToRead: null,
+    read: null,
+    none: null,
   };
   render() {
     return (
