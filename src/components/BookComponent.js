739947401bd7a9d79dde914cf30fe6ee1e62303a
prpwien <mail@prp.wien>
2021-05-13 12:09:57 +0200

    ✨ comma after displayed authors

--- src/components/BookComponent.js
+++ src/components/BookComponent.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { BookShelfChangerComponent } from "./BookShelfChangerComponent";
 
-/* 
-  TODO: 
-  1. Get data for the books
-*/
-
 export const BookComponent = ({ title, authors, thumbnail, infoLink }) => {
   return (
     <li>
       <div className="book">
         <div className="book-top">
           <div
-            oncClick={() => window.open("{infoLink}", "_blank")}
             className="book-cover"
             style={{
               width: 128,
@@ -24,9 +18,12 @@ export const BookComponent = ({ title, authors, thumbnail, infoLink }) => {
         </div>
         <div className="book-title">{title}</div>
         <div className="book-authors">
-          {/*TODO: ADD AUTHOR SPACING*/}
-          {authors.map((author) => {
-            return `${author} `;
+          {authors.map(function (author, index) {
+            return (
+              <span key={`demo_snap_${index}`}>
+                {(index ? ", " : "") + author}
+              </span>
+            );
           })}
         </div>
       </div>
