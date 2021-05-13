2a71272d200cd8bae1e5875dbeb5574b1de1d054
prpwien <mail@prp.wien>
2021-05-13 12:10:34 +0200

    🔥 cleanup

--- src/components/BookShelfChangerComponent.js
+++ src/components/BookShelfChangerComponent.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 /* 
   TODO: 
   - [ ] create moveTo(currentlyReading/wantToRead/read/none) function
 */
-export class BookShelfChangerComponent extends Component {
-  render() {
-    return (
-      <div className="book-shelf-changer">
-        <select>
-          <option value="move" disabled>
-            Move to...
-          </option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    );
-  }
-}
+export const BookShelfChangerComponent = () => {
+  return (
+    <div className="book-shelf-changer">
+      <select>
+        <option value="move" disabled>
+          Move to...
+        </option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
+      </select>
+    </div>
+  );
+};
 
 export default BookShelfChangerComponent;
