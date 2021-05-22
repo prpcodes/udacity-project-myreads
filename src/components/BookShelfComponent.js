836ae4a5d90c18b4db1bff48bf5ffc6911f412f5
prpwien <mail@prp.wien>
2021-05-22 23:00:07 +0200

    🎨 restructured code

--- src/components/BookShelfComponent.js
+++ src/components/BookShelfComponent.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { BookComponent } from "./BookComponent";
 
-export const BookShelfComponent = ({ books, title, shelf }) => {
+export const BookShelfComponent = ({ title, books }) => {
   return (
     <div className="bookshelf">
       <h2 className="bookshelf-title">{title}</h2>
       <div className="bookshelf-books">
         <ol className="books-grid">
           {books.map((book) => {
-            return (
-              book.shelf === shelf && <BookComponent key={book.id} {...book} />
-            );
+            return <BookComponent key={book.id} {...book} />;
           })}
         </ol>
       </div>
