d39e393078d132f18e8bc3d06c3072daa5cbcaa8
prpwien <mail@prp.wien>
2021-05-22 22:59:47 +0200

    🎨 restructured code

--- src/components/BookShelfChangerComponent.js
+++ src/components/BookShelfChangerComponent.js
@@ -1,62 +1,32 @@
-import React, { useState } from "react";
-/* 
-  TODO: 
-  - [ ] create moveTo(currentlyReading/wantToRead/read/none) function
-*/
-export const BookShelfChangerComponent = ({ id, shelf }) => {
-  function handleMoveBook(book) {
-    switch (book) {
-      case "currentlyReading":
-        console.log([id, shelf]);
-        break;
-      case "wantToRead":
-        console.log([id, shelf]);
-        break;
-      case "read":
-        console.log([id, shelf]);
-        break;
+import React from "react";
+import PropTypes from "prop-types";
 
-      case "none":
-        console.log([id, shelf]);
-        break;
+//API
+import * as BooksAPI from "../BooksAPI";
 
-      default:
-        console.log(book);
-        break;
-    }
+export const BookShelfChangerComponent = ({ value = "none", id }) => {
+  function handleChange(e) {
+    BooksAPI.update({ id: id }, e.target.value).then(() =>
+      window.location.reload(),
+    );
   }
 
   return (
     <div className="book-shelf-changer">
-      <select>
+      <select value={value} onChange={handleChange}>
         <option value="move" disabled>
           Move to...
         </option>
-        <option
-          onClick={() => handleMoveBook([id, "currentlyReading"])}
-          value="currentlyReading"
-        >
-          Currently Reading
-        </option>
-        <option
-          onClick={() => handleMoveBook([id, "wantToRead"])}
-          value="wantToRead"
-        >
-          Want to Read
-        </option>
-        <option 
-          onClick={() => handleMoveBook([id, "read"])} 
-          value="read">
-          Read
-        </option>
-        <option 
-          onClick={() => handleMoveBook([id, "none"])} 
-          value="none">
-          None
-        </option>
+        <option value="currentlyReading">Currently Reading</option>
+        <option value="wantToRead">Want to Read</option>
+        <option value="read">Read</option>
+        <option value="none">None</option>
       </select>
     </div>
   );
+};
+BookShelfChangerComponent.propTypes = {
+  value: PropTypes.string,
 };
 
 export default BookShelfChangerComponent;
